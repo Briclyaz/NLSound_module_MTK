69b7e0673cbfe6ab00bab96dd0cc82f3b85b1a57
Briclyaz <prorok.new@mail.ru>
2021-08-16 12:58:59 +0300

    Enable HIGH PERF MODE

--- service.sh
+++ service.sh
@@ -1,6 +1,11 @@
-# Kill audioserver PID if it exists already
-(
-sleep 3
-SERVERPID=$(pidof audioserver)
-[ "$SERVERPID" ] && kill $SERVERPID
-)&
+#!/system/bin/sh
+
+SERVICE=$MODPATH/service
+
+FORCING=$(find /sys/module -name high_perf_mode)
+
+chmod 0777 $SERVICE
+
+busybox echo "1" > $FORCING
+
+setprop mic.volume 7
